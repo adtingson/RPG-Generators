19f9532f62863b7124bc0e5d1932fe8e6b58353b
Albert Tingson <tingsonalbert@gmail.com>
2026-02-22 03:56:42 +0800

    5TD Update
    I am expanding my Generator Page to accommodate the tables and ideas from Five Torches Deep.

---
+++ Generators/5td-PCStat.js
@@ -0,0 +1,79 @@
+/*
+This is based on the Five Torches Deep Book
+*/
+
+function RaceStats(race) {
+    /*
+    "race" values
+    1 - human
+    2 - dwarf
+    3 - elf
+    4 - halfling
+    */
+    const statArray = [];
+    if (race==1) {
+        for (i=0; i<6; i++) {
+            var roll1=Math.floor(Math.random()*6)+1;
+            var roll2=Math.floor(Math.random()*6)+1;
+            var roll3=Math.floor(Math.random()*6)+1;
+            var rollTotal=roll1+roll2+roll3;
+            var resultPush = "<b>" + rollTotal.toString() + "</b> (" + roll1.toString() + " + " + roll2.toString() + " + "  + roll3.toString() + ")";
+            statArray.push(resultPush);
+        }
+        document.getElementById("strScore").innerHTML = statArray[0];
+        document.getElementById("dexScore").innerHTML = statArray[1];
+        document.getElementById("conScore").innerHTML = statArray[2];
+        document.getElementById("intScore").innerHTML = statArray[3];
+        document.getElementById("wisScore").innerHTML = statArray[4];
+        document.getElementById("chaScore").innerHTML = statArray[5];
+        document.getElementById("additionalRule").innerHTML = "You may swap scores twice.";
+    }
+    if (race==2) {
+        for (i=0; i<4; i++) {
+            var roll1=Math.floor(Math.random()*6)+1;
+            var roll2=Math.floor(Math.random()*6)+1;
+            var rollTotal=roll1+roll2+3;
+            var resultPush = "<b>" + rollTotal.toString() + "</b> (" + roll1.toString() + " + " + roll2.toString() + " + 3)";
+            statArray.push(resultPush);
+        }
+        document.getElementById("strScore").innerHTML = "<b>13</b>";
+        document.getElementById("dexScore").innerHTML = statArray[0];
+        document.getElementById("conScore").innerHTML = "<b>13</b>";
+        document.getElementById("intScore").innerHTML = statArray[1];
+        document.getElementById("wisScore").innerHTML = statArray[2];
+        document.getElementById("chaScore").innerHTML = statArray[3];
+        document.getElementById("additionalRule").innerHTML = "Class Restrictions: Thief 13+ DEX; Mage 13+ INT";
+    }
+    if (race==3) {
+        for (i=0; i<4; i++) {
+            var roll1=Math.floor(Math.random()*6)+1;
+            var roll2=Math.floor(Math.random()*6)+1;
+            var rollTotal=roll1+roll2+3;
+            var resultPush = "<b>" + rollTotal.toString() + "</b> (" + roll1.toString() + " + " + roll2.toString() + " + 3)";
+            statArray.push(resultPush);
+        }
+        document.getElementById("strScore").innerHTML = statArray[0];
+        document.getElementById("dexScore").innerHTML = "<b>13</b>";
+        document.getElementById("conScore").innerHTML = statArray[1];
+        document.getElementById("intScore").innerHTML = "<b>13</b>";
+        document.getElementById("wisScore").innerHTML = statArray[2];
+        document.getElementById("chaScore").innerHTML = statArray[3];
+        document.getElementById("additionalRule").innerHTML = "Class Restrictions: Warrior 13+ STR; Zealot 13+ WIS";
+    }
+    if (race==4) {
+        for (i=0; i<4; i++) {
+            var roll1=Math.floor(Math.random()*6)+1;
+            var roll2=Math.floor(Math.random()*6)+1;
+            var rollTotal=roll1+roll2+3;
+            var resultPush = "<b>" + rollTotal.toString() + "</b> (" + roll1.toString() + " + " + roll2.toString() + " + 3)";
+            statArray.push(resultPush);
+        }
+        document.getElementById("strScore").innerHTML = statArray[0];
+        document.getElementById("dexScore").innerHTML = statArray[1];
+        document.getElementById("conScore").innerHTML = statArray[2];
+        document.getElementById("intScore").innerHTML = statArray[3];
+        document.getElementById("wisScore").innerHTML = "<b>13</b>";
+        document.getElementById("chaScore").innerHTML = "<b>13</b>";
+        document.getElementById("additionalRule").innerHTML = "Class Restrictions: Warrior 13+ STR; Mage 13+ INT";
+    }
+}
